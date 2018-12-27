84077a98a4a91d33e78f9239dd5dc1627fcc64c3
Alex Kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-12-26 23:18:17 -0800

    Navigation bar code

---
+++ resources/top_nav.php
@@ -0,0 +1,6 @@
+<li class="navigation__item"><a href="index.php" class="navigation__link"><span>00</span> Home Page</a></li>
+<li class="navigation__item"><a href="about.php" class="navigation__link"><span>01</span> About</a></li>
+<li class="navigation__item"><a href="resume.php" class="navigation__link"><span>02</span> Resume</a></li>
+<li class="navigation__item"><a href="business.php" class="navigation__link"><span>03</span> My Business</a></li>
+<li class="navigation__item"><a href="finance.php" class="navigation__link"><span>04</span> Finances</a></li>
+<li class="navigation__item"><a href="contact.php" class="navigation__link"><span>05</span> Contact</a></li>
