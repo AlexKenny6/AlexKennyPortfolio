caa0a2d292c987bf9e843a43132fa41c7da42ec1
Alex Kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-12-26 23:19:37 -0800

    Footer.php

---
+++ resources/front/footer.php
@@ -0,0 +1,142 @@
+
+    <section class="section-projects" id="projects">
+            <div class="u-center-text u-margin-bottom-big">
+                <h2 class="heading-secondary">Most Recent Projects</h2>
+            </div>
+            <div class="row">
+                <div class="col-1-of-3">
+                    <div class="card">
+                        <div class="card__side card__side--front">
+                            <div class="card__picture card__picture--1">
+                                &nbsp;
+                            </div>
+                            <h4 class="card__heading">
+                                <span class="card__heading-span card__heading-span--1">Pacwest Software Development</span>
+                            </h4>
+                            <br>
+                            <div class="card__details">
+                                <ul>
+                                    <li>Software Dev LLC in WA State</li>
+                                    <li>Used: HTML, CSS, JS, PHP</li>
+                                    <li>CSS Flex/Grid Foundation</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card__side card__side--back card__side--back-1">
+                            <div class="card__cta">
+                                <div class="card__price-box">
+                                    <p class="card__price-only">To Visit the Site:</p>
+                                    <!--<p class="card__price-value">$297</p>-->
+                                </div>
+                                <a href="https://pacwestsoftware.com/index.php" target="_blank" class="btn btn--white">Click Here!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of card 1 -->
+
+                <div class="col-1-of-3">
+                    <div class="card">
+                        <div class="card__side card__side--front">
+                            <div class="card__picture card__picture--2">
+                                &nbsp;
+                            </div>
+                            <h4 class="card__heading">
+                                <span class="card__heading-span card__heading-span--2">Nerdcore Toys & Collectibles</span>
+                            </h4>
+                            <div class="card__details">
+                                <ul>
+                                    <li>Card and Games Company in Ellensburg, WA</li>
+                                    <li>Used: HTML, CSS, JS, PHP, Bootstrap</li>
+                                    <li>PHP Backend allows for product creation/editing, add/edit categories, view purchase reports</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card__side card__side--back card__side--back-2">
+                            <div class="card__cta">
+                                <div class="card__price-box">
+                                    <p class="card__price-only">To Visit the Site:</p>
+                                    <!--p class="card__price-value">$297</p>-->
+                                </div>
+                                <a href="https://www.nerdcoretoys.com/" class="btn btn--white" target="_blank">Click Here!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of card 2 -->
+
+                <div class="col-1-of-3">
+                    <div class="card">
+                        <div class="card__side card__side--front">
+                            <div class="card__picture card__picture--3">
+                                &nbsp;
+                            </div>
+                            <h4 class="card__heading">
+                                <span class="card__heading-span card__heading-span--3">Pocket Puppy Hotel</span>
+                            </h4>
+                            <div class="card__details">
+                                <ul>
+                                    <li>Toy and Miniature Dog Boarding Company in Duvall, WA</li>
+                                    <li>Built with Squarespace and has custom css</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card__side card__side--back card__side--back-3">
+                            <div class="card__cta">
+                                <div class="card__price-box">
+                                    <p class="card__price-only">To Visit the Site:</p>
+                                    <!--<p class="card__price-value">$297</p>-->
+                                </div>
+                                <a href="https://www.pocketpuppyhotel.com/" class="btn btn--white" target="_blank">Click Here!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of card 3 -->
+            </div>
+            <!-- end of row -->
+
+            <!--<div class="u-center-text u-margin-bottom-big">
+                <a href="#" class="btn btn--green">Discover all projects</a>
+            </div>-->
+        </section>
+
+
+    <footer class="footer">
+        <div class="footer__logo-box">
+            <picture class="footer__logo">
+                <source srcset="img/AK-Logo@1x.png 1x, img/AK-Logo@2x.png 2x" 
+                    media="(max-width: 37.5em)">
+                <img srcset="img/AK-Logo@1x.png 1x, img/AK-Logo@2x.png 2x" alt="logo" class="footer__logo">
+                <!-- above code lets the browser pick the best of two photos to use -->
+            </picture>
+        </div>
+        <div class="row">
+            <div class="col-1-of-2">
+                <div class="footer__navigation">
+                    <ul class="footer__list">
+                        <li class="footer__item"><a href="../../../index.php" class="footer__link">Home</a></li>
+                        <li class="footer__item"><a href="../../../about.php" class="footer__link">About</a></li>
+                        <li class="footer__item"><a href="../../../resume.php" class="footer__link">Resume</a></li>
+                        <li class="footer__item"><a href="../../../business.php" class="footer__link">My Business</a></li>
+                        <li class="footer__item"><a href="../../../finance.php" class="footer__link">Finance</a></li>
+                        <li class="footer__item"><a href="../../../contact.php" class="footer__link">Contact Me</a></li>
+                        <br>
+                        <li class="footer__item"><a class="footer__link" href="https://www.instagram.com/alex_kenny_6/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                        <li class="footer__item"><a class="footer__link" href="https://www.facebook.com/alex.kenny.507" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                        <li class="footer__item"><a class="footer__link" href="https://www.linkedin.com/in/alex-kenny-032543124/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                        <li class="footer__item"><a class="footer__link" href="https://twitter.com/Im_Kenny_" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
+                        <li class="footer__item"><a class="footer__link" href="https://github.com/AlexKenny6" target="_blank"><i class="fab fa-github"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-1-of-2">
+                <p class="footer__copyright">
+                    Built by <a href="https://pacwestsoftware.com/index.php" class="footer__link">Pacwest Software Development LLC</a>. Copyright &copy; Pacwest Software Development LLC. All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+
+    </body>
+</html>
