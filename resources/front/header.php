b224daf43c6572def29bee4f0fdeb71792cd2b9c
Alex Kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-12-26 23:19:20 -0800

    Header.php

---
+++ resources/front/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+
+        <link rel="stylesheet" href="../../../css/icon-fonts.css">
+        <link rel="stylesheet" href="../../../css/style.css">
+        <link rel="shortcut icon" type="image/png" href="../../../img/ak-fav.ico">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        
+        <title>Alex Kenny's Website</title>
+    </head>
+    <body>
+        
